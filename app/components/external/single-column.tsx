b6c3419b4e1c28a3af6baf78d76fada56576635a
teguhkurnia <teguhkurniaixi@gmail.com>
2025-05-08 14:07:22 +0700

    Add SingleColumn component for single-column layout

---
+++ app/components/external/single-column.tsx
@@ -0,0 +1,39 @@
+import { Element, Frame, useNode } from '@craftjs/core'
+import React from 'react'
+import Container from './container'
+import Text from './text'
+
+interface ExternalSingleColumnProps {
+  children?: React.ReactNode
+}
+
+export const SingleColumnChildren: React.FC<ExternalSingleColumnProps> = ({
+  children,
+}) => {
+  const {
+    connectors: { connect },
+  } = useNode()
+  return (
+    <div
+      ref={(ref) => {
+        if (ref) connect(ref)
+      }}
+      className='py-2 w-full bg-red-200'
+    >
+      {children ?? <div className='m-2 bg-gray-200 p-2'>Single Column</div>}
+    </div>
+  )
+}
+
+const SingleColumn: React.FC = ({}) => {
+  const {
+    connectors: { connect },
+  } = useNode()
+  return (
+    <Container>
+      <Element id='single-column' is={SingleColumnChildren} canvas></Element>
+    </Container>
+  )
+}
+
+export default SingleColumn
