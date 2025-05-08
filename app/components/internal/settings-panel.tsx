76163880cebb144a1d8da41102f08a7883ecb638
teguhkurnia <teguhkurniaixi@gmail.com>
2025-05-08 21:37:13 +0700

    Implement dynamic settings rendering in SettingsPanel component

--- app/components/internal/settings-panel.tsx
+++ app/components/internal/settings-panel.tsx
@@ -1,7 +1,32 @@
+import { useEditor } from '@craftjs/core'
 import React from 'react'
 
 const SettingsPanel = () => {
-  return <div className='border-l p-2.5'>SettingsPanel</div>
+  const { selected } = useEditor((state) => {
+    const [currentNodeId] = state.events.selected
+    let selected
+
+    if (currentNodeId) {
+      selected = {
+        id: currentNodeId,
+        name: state.nodes[currentNodeId].data.name,
+        settings:
+          state.nodes[currentNodeId].related &&
+          state.nodes[currentNodeId].related.settings,
+      }
+    }
+
+    return {
+      selected,
+    }
+  })
+
+  return (
+    <div className='border-l p-2.5'>
+      SettingsPanel
+      {selected?.settings && React.createElement(selected.settings)}
+    </div>
+  )
 }
 
 export default SettingsPanel
