eceea83b4c957b5f89714f5439b7121fb9ee0831
teguhkurnia <teguhkurniaixi@gmail.com>
2025-05-08 18:57:20 +0700

    Refactor Home component to integrate zoom functionality and clean up commented code

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { Editor, Element, Frame } from '@craftjs/core'
+import { Editor, Element, Frame, useEditor } from '@craftjs/core'
 import ComponentPicker from './components/internal/component-picker'
 import Navbar from './components/internal/navbar'
 import Text from './components/external/text'
@@ -9,11 +9,14 @@ import SingleColumn, {
 } from './components/external/single-column'
 import DoubleColumn from './components/external/double-clumn'
 import SettingsPanel from './components/internal/settings-panel'
+import { Zoom } from '@react-zoom/react-zoom-in-out'
+import { useEffect, useRef } from 'react'
+import { TransformComponent, TransformWrapper } from 'react-zoom-pan-pinch'
 
 export default function Home() {
   return (
     <div className='h-screen w-screen'>
-      <Navbar />
+      {/* <Navbar /> */}
       <Editor
         resolver={{
           Text,
@@ -22,17 +25,29 @@ export default function Home() {
           SingleColumnChildren,
           DoubleColumn,
         }}
+        onNodesChange={(q) => {
+          console.log('Nodes changed', q.serialize())
+        }}
       >
         <div className='h-full flex'>
           <ComponentPicker />
-          <div className='bg-white flex-1'>
-            <Frame>
-              <Element is={Container} canvas className='h-full'>
+          {/* 
+          <TransformWrapper initialScale={0.9} centerOnInit minScale={0.4}>
+            <TransformComponent
+              wrapperStyle={{ width: '100%', height: '100%' }}
+            > */}
+          <div className='bg-white flex-1 relative overflow-auto container'>
+            <Frame
+              data={`{"ROOT":{"type":{"resolvedName":"Container"},"isCanvas":true,"props":{"className":"h-full"},"displayName":"Container","custom":{},"hidden":false,"nodes":["PynkEc82DB","0bjGLVnUAO"],"linkedNodes":{}},"PynkEc82DB":{"type":{"resolvedName":"Text"},"isCanvas":false,"props":{"text":"Hello World","fontSize":12},"displayName":"Text","custom":{},"parent":"ROOT","hidden":false,"nodes":[],"linkedNodes":{}},"0bjGLVnUAO":{"type":{"resolvedName":"Text"},"isCanvas":false,"props":{"text":"Hello World 2","fontSize":24},"displayName":"Text","custom":{},"parent":"ROOT","hidden":false,"nodes":[],"linkedNodes":{}}}`}
+            >
+              {/* <Element is={Container} canvas className='h-full'>
                 <Text text='Hello World' fontSize={12} />
                 <Text text='Hello World 2' fontSize={24} />
-              </Element>
+              </Element> */}
             </Frame>
           </div>
+          {/* </TransformComponent>
+          </TransformWrapper> */}
 
           <SettingsPanel />
         </div>
