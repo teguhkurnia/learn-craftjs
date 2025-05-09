f6ddf8c5e748cd9374898c5f793c07ca31138140
teguhkurnia <teguhkurniaixi@gmail.com>
2025-05-09 22:49:03 +0700

    Refactor Navbar component to implement screen size selection functionality

--- app/components/internal/navbar.tsx
+++ app/components/internal/navbar.tsx
@@ -1,7 +1,38 @@
+import useScreenState from '@/app/stores/screen-state'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
 import React from 'react'
 
 const Navbar = () => {
-  return <div className='border-b p-2.5'>Navbar</div>
+  const { currentScreen, setCurrentScreen } = useScreenState()
+  return (
+    <div className='border-b p-2.5 grid grid-cols-3'>
+      <div className=''></div>
+      <div className='flex justify-center items-center'>
+        <Select
+          defaultValue={currentScreen}
+          onValueChange={(value) => {
+            setCurrentScreen(value as 'desktop' | 'tablet' | 'mobile')
+          }}
+        >
+          <SelectTrigger className='w-[180px]'>
+            <SelectValue placeholder='Screen Size' />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value='desktop'>Desktop</SelectItem>
+            <SelectItem value='tablet'>Tablet</SelectItem>
+            <SelectItem value='mobile'>Mobile</SelectItem>
+          </SelectContent>
+        </Select>
+      </div>
+      <div className=''></div>
+    </div>
+  )
 }
 
 export default Navbar
